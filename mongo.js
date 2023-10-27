d5c0c82e9bdd6d3f31011173efb0cd83414adbc9
HaHavn94 <hanna.myllarinen7994@gmail.com>
2023-10-27 18:30:01 +0300

    i

---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+    `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    date: Date,
+    important: Boolean,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+    name: "HA",
+    number: "123"
+})
+
+/*
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
+*/
+
+Person.find({}).then(result => {
+    result.forEach(person => {
+        console.log(person)
+    })
+    mongoose.connection.close()
+})
